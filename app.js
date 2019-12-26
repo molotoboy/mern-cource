22b16e0e5d975415b2f537bf2051205976eab01f
molotoboy <molotoboy@gmail.com>
2019-12-26 22:55:13 +0600

    add mongo

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+  } catch (e) {
+    console.log('Server Error: ', e.message);
+    process.exit(1);
+  }
+}
+
+start();
 
 app.listen(PORT, () =>
   console.log(`Server started on http://localhost:${PORT}`)
