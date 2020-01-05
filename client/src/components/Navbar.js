df8a628d5c209a15ec5b4fc6ce976e01c30d3817
molotoboy <molotoboy@gmail.com>
2020-01-06 00:25:45 +0600

    add Navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export const Navbar = () => {
+  const history = useHistory()
+  const auth = useContext(AuthContext)
+
+  const logoutHandler = event => {
+    event.preventDefault()
+    auth.logout()
+    history.push('/')
+  }
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <a href='/' className='brand-logo'>
+          ShortLinks
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <NavLink to='/create'>create</NavLink>
+          </li>
+          <li>
+            <NavLink to='/links'>links</NavLink>
+          </li>
+          <li>
+            <a href='/' onClick={logoutHandler}>
+              logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
