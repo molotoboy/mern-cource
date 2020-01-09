e6d6a460fa628ff5788d55b4a4f436e9556f69dc
molotoboy <molotoboy@gmail.com>
2020-01-09 21:23:59 +0600

    add Links

---
+++ models/Link.js
@@ -0,0 +1,11 @@
+const { Schema, model, Types } = require('mongoose')
+const schema = new Schema({
+  from: { type: String, required: type },
+  to: { type: String, required: type, unique: true },
+  code: { type: String, required: type, unique: true },
+  date: { type: Date, default: Date.now },
+  clicks: { type: Number, default: 0 },
+  owner: { type: Types.ObjectId, ref: 'User' }
+})
+
+module.exports = model('Link', schema)
